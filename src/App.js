52f8f1ab1db064d02fc71880d9ed24df7d9614fa
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2021-11-15 10:25:14 +0500

    15-11-2021

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { useMemo, useState, useEffect } from "react";
+import { useFilters } from "react-table";
 import axios from "axios";
-
-import Table from "./Table";
 import "./App.css";
+import Table from "./Table";
+// Custom component to render Genres 
+const Genres = ({ values }) => {
+  // Loop through the array and create a badge-like component instead of a comma-separated string
+  return (
+    <>
+      {values.map((genre, idx) => {
+        return (
+          <span key={idx} className="badge">
+            {genre}
+          </span>
+        );
+      })}
+    </>
+  );
+};
 
+function App() {
+  const [data, setData] = useState([]);
+  const [filterInput, setFilterInput] = useState("");
+  
 
-
-const [data, setData] = useState([]);
-
+ const handleFilterChange = (e) => {
+   const value = e.target.value || undefined;
+ // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+   setFilterInput(value);
+ };
+  // Using useEffect to call the API once mounted and set the data
   useEffect(() => {
     (async () => {
       const result = await axios("https://api.tvmaze.com/search/shows?q=snow");
       setData(result.data);
     })();
   }, []);
-
- 
-
-  function App(){
+  console.log(data)
   const columns = useMemo(
     () => [
       {
@@ -27,13 +46,13 @@ const [data, setData] = useState([]);
         columns: [
           {
             Header: "Name",
-            accessor: "show.name"
+            accessor: "show.name",
           },
           {
             Header: "Type",
-            accessor: "show.type"
-          }
-        ]
+            accessor: "show.type",
+          },
+        ],
       },
       {
         // Second group - Details
@@ -42,31 +61,50 @@ const [data, setData] = useState([]);
         columns: [
           {
             Header: "Language",
-            accessor: "show.language"
+            accessor: "show.language",
           },
           {
             Header: "Genre(s)",
-            accessor: "show.genres"
+            accessor: "show.genres",
+            // Cell: ({ cell: { value } }) => <Genres values={value} />,
           },
           {
             Header: "Runtime",
-            accessor: "show.runtime"
+            accessor: "show.runtime",
+            Cell: ({ cell: { value } }) => {
+              const hour = Math.floor(value / 60);
+              const min = Math.floor(value % 60);
+              return (
+                <>
+                  {hour > 0 ? `${hour} hr${hour > 1 ? "s" : ""} ` : ""}
+                  {min > 0 ? `${min} min${min > 1 ? "s" : ""}` : ""}
+                </>
+              );
+            }
+          
           },
           {
             Header: "Status",
-            accessor: "show.status"
-          }
-        ]
-      }
+            accessor: "show.status",
+          },
+        ],
+      },
     ],
     []
   );
+  
+ 
 
+  return (
+    <div className="App">
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search name"}
+      />
+      <Table columns={columns} data={data} />
+    </div>
+  );
+}
 
-
-
-
-
-
-  }
-  export default App;
+export default App;
--- src/Table.js
+++ src/Table.js
@@ -1,81 +1,60 @@
-import React, { useState } from "react";
-import { useTable, useFilters, useSortBy } from "react-table";
-
+import React from "react";
+import { useTable,useFilters } from "react-table";
 
 export default function Table({ columns, data }) {
-  const [filterInput, setFilterInput] = useState("");
-  // Use the state and functions returned from useTable to build your UI
   
+
+  // Use the useTable Hook to send the columns and data to build the table
   
   const {
-    getTableProps,// table props from react-table
+    getTableProps, // table props from react-table
     getTableBodyProps, // table body props from react-table
     headerGroups, // headerGroups, if your table has groupings
-    rows,// rows for the table based on the data passed
+    rows, // rows for the table based on the data passed
     prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
-    setFilter
-  } = useTable(
-    {
-      columns,
-      data
-    },
+    setFilter,
+  } = useTable({
+    columns,
+    data,
+  },
+      useFilters,
+      );
+    
+      
 
+  /* 
+    Render the UI for your table
+    - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
+  */
 
     
-    useFilters,
-    useSortBy
-  );
 
-  const handleFilterChange = e => {
-    const value = e.target.value || undefined;
-    setFilter("show.name", value);
-    setFilterInput(value);
-  };
-
-  // Render the UI for your table
   return (
-    <>
-      <input
-        value={filterInput}
-        onChange={handleFilterChange}
-        placeholder={"Search name"}
-      />
-      <table {...getTableProps()}>
-        <thead>
-          {headerGroups.map(headerGroup => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map(column => (
-                <th
-                  {...column.getHeaderProps(column.getSortByToggleProps())}
-                  className={
-                    column.isSorted
-                      ? column.isSortedDesc
-                        ? "sort-desc"
-                        : "sort-asc"
-                      : ""
-                  }
-                >
-                  {column.render("Header")}
-                </th>
-              ))}
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              })}
             </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+          );
+        })}
+
+
+      
+      </tbody>
+    </table>
   );
 }
